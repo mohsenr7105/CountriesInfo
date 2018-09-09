11bb670a04d4f24cba62c8f7158128071cd64aa1
Mohsen <mohsenr7105@gmail.com>
2018-09-09 15:22:49 +0430

    showing more info about chosen country

--- app/src/main/java/ir/mohsenr7105/countriesinfo/Country.java
+++ app/src/main/java/ir/mohsenr7105/countriesinfo/Country.java
@@ -1,16 +1,16 @@
 package ir.mohsenr7105.countriesinfo;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.util.Log;
-
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.Locale;
 
 public class Country {
     private static final String LOG = Country.class.getSimpleName();
 
-    private String name, farsiName, alpha2Code;
+    private String name, farsiName, nativeName, alpha2Code, alpha3Code, capitalNativeName;
+    private Long population, area;
+
+    Country(){
+
+    }
 
     Country(String name, String farsiName, String alpha2Code){
         this.name = name;
@@ -30,6 +30,26 @@ public class Country {
         this.alpha2Code = alpha2Code;
     }
 
+    public void setAlpha3Code(String alpha3Code) {
+        this.alpha3Code = alpha3Code;
+    }
+
+    public void setNativeName(String nativeName) {
+        this.nativeName = nativeName;
+    }
+
+    public void setCapitalNativeName(String capitalNativeName) {
+        this.capitalNativeName = capitalNativeName;
+    }
+
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
+
+    public void setArea(Long area) {
+        this.area = area;
+    }
+
     public String getName() {
         return name;
     }
@@ -40,5 +60,32 @@ public class Country {
 
     public String getAlpha2Code() {
         return alpha2Code;
+    }
+
+    public String getAlpha3Code() {
+        return alpha3Code;
+    }
+
+    public String getNativeName() {
+        return nativeName;
+    }
+
+    public String getCapitalNativeName() {
+        return capitalNativeName;
+    }
+
+    public Long getPopulation() {
+        return population;
+    }
+
+    public Long getArea() {
+        return area;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.getDefault(),"%1$s %2$s %3$s %4$s %5$s %6$s %7$d %8$d",
+                getName(), getNativeName(), getFarsiName(), getAlpha2Code(), getAlpha3Code(),
+                getCapitalNativeName(), getPopulation(), getArea());
     }
 }
--- app/src/main/java/ir/mohsenr7105/countriesinfo/MainActivity.java
+++ app/src/main/java/ir/mohsenr7105/countriesinfo/MainActivity.java
@@ -1,11 +1,16 @@
 package ir.mohsenr7105.countriesinfo;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.text.Html;
+import android.text.Spanned;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import org.json.JSONArray;
@@ -56,8 +61,28 @@ public class MainActivity extends AppCompatActivity {
         listCountries = findViewById(R.id.list_countries);
         countriesAdapter = new CountriesAdapter(this, countriesList);
         listCountries.setAdapter(countriesAdapter);
+        listCountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Country country = (Country) adapterView.getItemAtPosition(i);
+                new RetrieveCountryTask().execute(country.getAlpha2Code());
+            }
+        });
 
         new RetrieveCountriesTask().execute();
+    }
+
+    public void showMoreInfoAlert(Country country){
+        String info = getString(R.string.html_country_more_info,
+                country.getName(), country.getNativeName(), country.getFarsiName(),
+                country.getAlpha2Code(), country.getAlpha3Code(), country.getCapitalNativeName(),
+                country.getPopulation(), country.getArea());
+        Spanned htmlText = Html.fromHtml(info);
+        new AlertDialog.Builder(this)
+                .setTitle("more info")
+                .setMessage(htmlText)
+                .setNeutralButton("ok", null)
+                .show();
     }
 
     private void enableHttpResponseCache() {
@@ -131,8 +156,72 @@ public class MainActivity extends AppCompatActivity {
                 ex.printStackTrace();
             } finally {
                 progressDialog.dismiss();
-                Log.d(LOG, countriesList.toString());
                 countriesAdapter.notifyDataSetChanged();
+            }
+        }
+    }
+
+    class RetrieveCountryTask extends AsyncTask<String, Void, String>{
+        private ProgressDialog progressDialog;
+
+        RetrieveCountryTask() {
+            progressDialog = new ProgressDialog(MainActivity.this);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog.setMessage("downloading");
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... alpha2Code) {
+            try {
+                URL url = new URL("https://restcountries.eu/rest/v2/alpha/" + alpha2Code[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception ex) {
+                Log.e(LOG, ex.getMessage());
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            if (response == null) {
+                progressDialog.dismiss();
+
+                return;
+            }
+//            Log.i(LOG, response);
+            Country country = new Country();
+            try {
+                JSONObject countryJson = (JSONObject) new JSONTokener(response).nextValue();
+                country.setName(countryJson.getString("name"));
+                country.setNativeName(countryJson.getString("nativeName"));
+                country.setFarsiName(countryJson.getJSONObject("translations").getString("fa"));
+                country.setAlpha2Code(countryJson.getString("alpha2Code"));
+                country.setAlpha3Code(countryJson.getString("alpha3Code"));
+                country.setCapitalNativeName(countryJson.getString("capital"));
+                country.setPopulation(countryJson.getLong("population"));
+                country.setArea(countryJson.getLong("area"));
+            } catch (JSONException ex) {
+                ex.printStackTrace();
+            } finally {
+                progressDialog.dismiss();
+                showMoreInfoAlert(country);
             }
         }
     }
